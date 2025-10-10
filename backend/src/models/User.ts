8ae07ad4611b0edb3517853a5860a2ab7262506c
emdhruu <thakkardhruti22@gmail.com>
2025-10-10 15:50:52 +0530

    added interface for user

--- backend/src/models/User.ts
+++ backend/src/models/User.ts
@@ -1,6 +1,7 @@
 import mongoose from "mongoose";
+import { IUser } from "../utils/interface";
 
-const userSchema = new mongoose.Schema(
+const userSchema = new mongoose.Schema<IUser>(
   {
     name: {
       type: String,
