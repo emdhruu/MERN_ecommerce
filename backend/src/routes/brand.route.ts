cd15922837fd46be3ebb119254e0d79d5524d6f7
emdhruu <thakkardhruti22@gmail.com>
2025-11-24 18:08:59 +0530

    brand route

---
+++ backend/src/routes/brand.route.ts
@@ -0,0 +1,18 @@
+import { createBrand, deleteBrandById, getAllBrands, getBrandBySlug, updateBrand } from "../controllers/brands.controller";
+import { isAdmin } from "../middleware/isAdmin";
+
+const express = require('express');
+
+const router = express.Router();
+
+// Only Admin can create, update, delete brands
+router.post('/create', isAdmin, createBrand);
+
+router.get('/all', getAllBrands);
+
+router.get('/:slug', getBrandBySlug);
+
+router.put('/update/:id', isAdmin, updateBrand);
+
+router.delete('/delete/:id', isAdmin, deleteBrandById);
+export default router;
