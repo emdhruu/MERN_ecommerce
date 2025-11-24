2989ce2192fb23dd693ec53e084c090836872039
emdhruu <thakkardhruti22@gmail.com>
2025-11-24 18:09:25 +0530

    slight changes

--- backend/src/routes/user.route.ts
+++ backend/src/routes/user.route.ts
@@ -5,8 +5,11 @@ import { isAdmin } from "../middleware/isAdmin";
 const router = express.Router();
 
 router.get("/usersList", isAdmin, getAllUsers);
+
 router.delete("/deleteUser/:id", isAdmin, deleteByIdUser);
+
 router.get("getUser/:id", getByIdUser);
+
 router.put("updateUser/:id", updateByIdUser);
 
 export default router;
