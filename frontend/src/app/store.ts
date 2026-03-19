f61eb88971ebb8d12744cdb217abaf0c85b6072d
emdhruu <thakkardhruti22@gmail.com>
2026-03-19 23:44:04 +0530

    added the missing middleware rtk query's

--- frontend/src/app/store.ts
+++ frontend/src/app/store.ts
@@ -7,6 +7,7 @@ export const store = configureStore({
         auth: authSlice,
         [apiSlice.reducerPath]: apiSlice.reducer,
     },
+    middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(apiSlice.middleware),
 })
 
 export type RootState = ReturnType<typeof store.getState>;
